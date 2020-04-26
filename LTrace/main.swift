12de00018c304efe28745f51dd4b97042594ad27
Luka Jankovic <lukjan1999@gmail.com>
2020-04-26 23:53:30 +0200

    Initial Commit

---
+++ LTrace/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LTrace
+//
+//  Created by Luka Jankovic on 2020-04-26.
+//  Copyright © 2020 Luka Jankovic. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
